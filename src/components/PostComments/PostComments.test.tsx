d8d47e244a89c6c029b23ffbbc14c09cc5c50902
Jonatas <jonatas.07022003@gmail.com>
2024-01-30 13:34:46 -0300

    Criando teste de inserção de dois comentarios

--- src/components/PostComments/PostComments.test.tsx
+++ src/components/PostComments/PostComments.test.tsx
@@ -1,10 +1,23 @@
 import { fireEvent, render, screen } from '@testing-library/react';
-import Post from '.';
 import PostComment from '.';
 
 describe('Teste para o componente PostComment', () => {
-    it('Deve renderizar o componente corretamente', () => {
+    test('Deve renderizar o componente corretamente', () => {
         render(<PostComment/>);
         expect(screen.getByText('Comentar')).toBeInTheDocument();
     });
-});
+
+    test('Deve inserir dois comentários', () => {
+        render(<PostComment/>)
+        const comentario = screen.getByPlaceholderText('Comentar')
+        const botaoAdd = screen.getByText('Comentar')
+
+        fireEvent.change(comentario, {target: {value:'Legal!'}})
+        fireEvent.click(botaoAdd)
+        expect(screen.getByText('Legal!')).toBeInTheDocument()
+
+        fireEvent.change(comentario, {target: {value:'Prefiro a Marvel'}})
+        fireEvent.click(botaoAdd)
+        expect(screen.getByText('Prefiro a Marvel')).toBeInTheDocument()
+    })
+})
